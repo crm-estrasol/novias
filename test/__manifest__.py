2dcf9038b3545c78e5a6851fac9650072299f2b5
Kevin del Campo <kcampo@estrasol.com.mx>
2020-03-11 11:11:26 -0600

    test commit

--- test/__manifest__.py
+++ test/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "Novias",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Funcionalidades extras novias de españa""",
 
     'description': """
-        Long description of module's purpose
+       Modulo afecta y agrega funcionalidades extras a Odoo para el flujo de novias de españa.
     """,
 
     'author': "Estrasol -Kevin Daniel del Campo",
-    'website': "http://www.yourcompany.com",
+    'website': "https://estrasol.com.mx/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
