d8bb214a61081ea0df2bc01d0aec86d2bcc595e2
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-06 16:20:35 -0500

    new d

--- intelli/models/department/department_area.py
+++ intelli/models/department/department_area.py
@@ -172,17 +172,18 @@ class Departent_Area(models.Model):
             data_end['extra_products'] = []
             ctr_1 = self.env['intelli.blind'].search(['&',('parent_tower.id','=',search[0].parent_tower),'&',('electronic.name','like','Control 1 Canal'),('style.name','like','Electrónica')])
             if ctr_1:
-                data_end['extra_products'].append( {
-                                                    'product_id': product.id,
-                                                    'product':product.name,
-                                                    'price':product.price,
-                                                    'style':product.style.name,
-                                                    'style_id': product.style.id,
-                                                    'electronic':product.electronic.name,
-                                                    'electronic_id': product.electronic.id,
-                                                    'image':product.blind,
-                                                    'images':[ image.image for image in product.images ]                   
-                                                } for product in ctr_1  )
+               for product in ctr_1: 
+                    data_end['extra_products'].append( {
+                                                        'product_id': product.id,
+                                                        'product':product.name,
+                                                        'price':product.price,
+                                                        'style':product.style.name,
+                                                        'style_id': product.style.id,
+                                                        'electronic':product.electronic.name,
+                                                        'electronic_id': product.electronic.id,
+                                                        'image':product.blind,
+                                                        'images':[ image.image for image in product.images ]                   
+                                                    }  )
         
         else: 
            data_end =  "null"     
