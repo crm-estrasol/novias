1e49c7350065b701deca1f3970ff5eafb976a90c
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-15 15:34:43 -0500

    WS pruebas

--- intelli/models/tower/blind.py
+++ intelli/models/tower/blind.py
@@ -75,9 +75,8 @@ class Blind(models.Model):
     def button_duplicate(self):
         blind_images = []
         for image in self.images:
-                new_image = image.copy({'parent_blind':False})
-                blind_images.append( (4, new_image.id) )
-
+            new_image = image.copy({'parent_blind':False})
+            blind_images.append( (4, new_image.id) )
         self.copy({'name':self.name+"(copia)",'images':blind_images})
         view_id = self.env.ref('intelli.tower_view_form_associate').id
         view = {
